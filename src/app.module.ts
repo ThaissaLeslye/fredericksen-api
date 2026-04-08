4db2578433004c4dc3d9677f2f7b9925f1d37ba4
thaissa <thaissa.leslye.tl@gmail.com>
2026-04-08 11:17:54 -0300

    add swagger doc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+//import { DatabaseModule } from './database/database.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { PerfilModule } from './perfil/perfil.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [UsuarioModule, PerfilModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fredericksen API')
+    .setDescription('Documentação do Fredericksen')
+    .setVersion('0.3.0')
+    // .addTag('usuarios')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
