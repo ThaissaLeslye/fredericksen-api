8eda8d102f3fcd2a2d76db6f8484216ac5f9c7f1
thaissa <thaissa.leslye.tl@gmail.com>
2026-04-19 10:47:33 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { DatabaseModule } from './database/database.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { PerfilModule } from './perfil/perfil.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { PerfilModule } from './modules/perfil/perfil.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [UsuarioModule, PerfilModule, PrismaModule],
