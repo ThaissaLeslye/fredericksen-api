af20f8cf83a882bd9a5c689a6dc8c81182ea08f6
thaissa <thaissa.leslye.tl@gmail.com>
2026-05-13 18:38:39 -0300

    corrige eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['documentation/', 'prisma.config.ts','eslint.config.mjs', 'dist/']
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
