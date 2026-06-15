19324f07c0a0dc4a91a52857b76a11a14be33817
thaissa <thaissa.leslye.tl@gmail.com>
2026-06-15 15:52:59 +0000

    test(config): exclude boilerplate modules and configurations from test coverage report

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '\\.module\\.ts$',
+    'main\\.ts$',
+    'common/config/.*',
+  ],
+};
+
+export default config;
