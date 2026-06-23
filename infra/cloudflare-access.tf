6a258c60e937a22198954fb4e2712573fd49f830
thaissa <thaissa.leslye.tl@gmail.com>
2026-06-23 17:48:31 +0000

    feat(infra): create bypass policy for api healthcheck path

--- infra/cloudflare-access.tf
+++ infra/cloudflare-access.tf
@@ -19,3 +19,24 @@ resource "cloudflare_access_policy" "api_allow_policy" {
     email_domain = ["fredericksen.local"]
   }
 }
+
+resource "cloudflare_access_application" "api_health_gateway" {
+  zone_id                   = var.cloudflare_zone_id
+  name                      = "Fredericksen API Healthcheck Public Path"
+  domain                    = "rick-api.tllo.app/health"
+  type                      = "self_hosted"
+  session_duration          = "2h"
+  auto_redirect_to_identity = false
+}
+
+resource "cloudflare_access_policy" "health_bypass_policy" {
+  application_id = cloudflare_access_application.api_health_gateway.id
+  zone_id        = var.cloudflare_zone_id
+  name           = "Bypass for External Monitoring Tools"
+  precedence     = 1
+  decision       = "bypass"
+
+  include {
+    everyone = true
+  }
+}
